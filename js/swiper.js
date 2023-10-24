40bf8f76f7e9be6a9a71eb0ac912c0a35cfb9f07
ivkovalevv <ivkovalevv@gmail.com>
2023-10-24 12:00:46 +0300

    add swiper

---
+++ js/swiper.js
@@ -0,0 +1,38 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // How sliders in block
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    
+    // Effect
+    effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+  
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    autoplay: {
+        delay: 1000,
+    },
+  });
